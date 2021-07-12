b9d23ab10640ea9491702c0717571c1fad323873
Ken Le <kle9597@gmail.com>
2021-07-11 18:29:12 -0700

    Init commit

---
+++ app/src/main/java/com/example/animelist/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.animelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    
+    public void clickFunction(View view) {
+        Log.i("Value", "ur mum");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
