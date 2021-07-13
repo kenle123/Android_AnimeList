39d4746bdb7ef49e9cab19c850874cef43542162
allenle123 <ale21100@gmail.com>
2021-07-12 21:44:05 -0700

    Created login page

--- app/src/main/java/com/example/animelist/MainActivity.java
+++ app/src/main/java/com/example/animelist/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     
     public void clickFunction(View view) {
 
         Log.i("Value", "chris broad in'nt");
+
+        EditText username = (EditText) findViewById(R.id.usernameInput);
+
+        EditText password = (EditText) findViewById(R.id.passwordInput);
     }
 
     @Override
