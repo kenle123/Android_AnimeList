b6d213932ca0521dd29b196e668c980061f656a2
Ken Le <kle9597@gmail.com>
2021-07-14 19:57:51 -0700

    Added intent to go to sign up screen when login button is pressed

--- app/src/main/java/com/example/animelist/MainActivity.java
+++ app/src/main/java/com/example/animelist/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.animelist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    
-    public void clickFunction(View view) {
-
-        Log.i("Value", "chris broad in'nt");
-
-        EditText username = (EditText) findViewById(R.id.usernameInput);
-
-        EditText password = (EditText) findViewById(R.id.passwordInput);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind variables
+        EditText username = (EditText) findViewById(R.id.usernameInput);
+        EditText password = (EditText) findViewById(R.id.passwordInput);
+
+        // Login button, for now only goes to sign up activity
+        Button loginBtn = (Button) findViewById(R.id.button);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), SignUpScreen.class);
+                    v.getContext().startActivity(intent);
+                }
+        });
     }
 }
