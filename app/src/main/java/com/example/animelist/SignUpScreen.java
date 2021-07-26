13675f43a3fb3028c00ce24aaff75543cf37202c
allenle123 <ale21100@gmail.com>
2021-07-25 18:05:03 -0700

    added back button in sign up page

--- app/src/main/java/com/example/animelist/SignUpScreen.java
+++ app/src/main/java/com/example/animelist/SignUpScreen.java
@@ -2,7 +2,13 @@ package com.example.animelist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SignUpScreen extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class SignUpScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
+
+        TextView goBackBtn = (TextView) findViewById(R.id.backToSignIn);
+        goBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 }
