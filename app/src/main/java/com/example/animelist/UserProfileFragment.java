15eeaba4c88115c7a189956d49103a642f481e06
allenle123 <ale21100@gmail.com>
2021-07-29 22:26:30 -0700

    added layout and TextViews to profile page

--- app/src/main/java/com/example/animelist/UserProfileFragment.java
+++ app/src/main/java/com/example/animelist/UserProfileFragment.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class UserProfileFragment extends Fragment {
@@ -22,6 +21,6 @@ public class UserProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false);
 
-
+        TextView t = (TextView) findViewById
     }
 }
