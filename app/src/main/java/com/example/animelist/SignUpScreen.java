8c4c1439753b12ccaf6124c003489884521813d0
Ken Le <kle9597@gmail.com>
2021-08-04 20:14:20 -0700

    Fixed sign up page logic. Checking if fields are empty and incorrect password are now working.

--- app/src/main/java/com/example/animelist/SignUpScreen.java
+++ app/src/main/java/com/example/animelist/SignUpScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +13,16 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class SignUpScreen extends AppCompatActivity {
+import java.util.logging.Logger;
 
+public class SignUpScreen extends AppCompatActivity {
+    EditText email;
+    EditText username;
+    EditText password;
+    EditText passwordConfirm;
+    EditText birthday;
+    EditText gender;
+    EditText phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,44 +30,27 @@ public class SignUpScreen extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up_screen);
 
         // bind variables
-        EditText email = (EditText) findViewById(R.id.id_email);
-        EditText username = (EditText) findViewById(R.id.id_username);
-        EditText password = (EditText) findViewById(R.id.id_password);
-        EditText passwordConfirm = (EditText) findViewById(R.id.id_password_comfirm);
-        EditText birthday = (EditText) findViewById(R.id.id_birthday);
-        EditText gender = (EditText) findViewById(R.id.id_gender);
-        EditText phone_number = (EditText) findViewById(R.id.id_phone_number);
+        email = (EditText) findViewById(R.id.id_email);
+        username = (EditText) findViewById(R.id.id_username);
+        password = (EditText) findViewById(R.id.id_password);
+        passwordConfirm = (EditText) findViewById(R.id.id_password_comfirm);
+        birthday = (EditText) findViewById(R.id.id_birthday);
+        gender = (EditText) findViewById(R.id.id_gender);
+        phoneNumber = (EditText) findViewById(R.id.id_phone_number);
 
         // Checks if there are any missing fields when user creates account
         Button createAccountBtn = (Button) findViewById(R.id.create_account_btn);
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(email.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
-                }
-                else if (username.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
-                }
-                else if (password.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
-                }
-                else if (passwordConfirm.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
-                }
-                else if (birthday.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
-                }
-                else if (gender.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
-                }
-                else if (phone_number.getText().toString().isEmpty()) {
-                    Toast.makeText(SignUpScreen.this, "Missing field(s)", Toast.LENGTH_SHORT).show();
+                boolean isFieldEmpty = checkIfFieldEmpty();
+                if(isFieldEmpty) {
+                    Toast.makeText(SignUpScreen.this, "Field(s) cannot be empty!", Toast.LENGTH_SHORT).show();
                 }
                 // Checks if password and confirm password is the same
-//                else if(password != passwordConfirm) {
-//                    Toast.makeText(SignUpScreen.this, "Confirm password not identical", Toast.LENGTH_SHORT).show();
-//                }
+                else if(!(password.getText().toString().equals(passwordConfirm.getText().toString()))) {
+                    Toast.makeText(SignUpScreen.this, "Confirm password not identical", Toast.LENGTH_SHORT).show();
+                }
                 else {
                     Intent intent = new Intent(v.getContext(), MainActivity.class);
                     v.getContext().startActivity(intent);
@@ -74,5 +66,16 @@ public class SignUpScreen extends AppCompatActivity {
                 v.getContext().startActivity(intent);
             }
         });
+    }
+
+    /**
+     * Checks if an EditText field is empty
+     * @return True if any of the EditText fields are empty, false otherwise
+     */
+    public boolean checkIfFieldEmpty() {
+        return email.getText().length() == 0 || username.getText().length() == 0 ||
+                password.getText().length() == 0 || passwordConfirm.getText().length() == 0 ||
+                birthday.getText().length() == 0 || gender.getText().length() == 0 ||
+                phoneNumber.getText().length() == 0;
     }
 }
