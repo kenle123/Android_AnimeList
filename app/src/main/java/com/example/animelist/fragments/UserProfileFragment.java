c756cb005bfa9625235c232e8e931588b3b19b91
allenle123 <ale21100@gmail.com>
2021-08-18 23:01:53 -0700

    finished user profile page

--- app/src/main/java/com/example/animelist/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/animelist/fragments/UserProfileFragment.java
@@ -1,32 +1,22 @@
 package com.example.animelist.fragments;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.service.autofill.CharSequenceTransformation;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.animelist.R;
 import com.example.animelist.SignInScreen;
-import com.example.animelist.SignUpScreen;
 import com.example.animelist.pojos.UserInformation;
 
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class UserProfileFragment extends Fragment {
@@ -39,6 +29,10 @@ public class UserProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Stores number of user animes in variable
+        int numberOfUserAnimes = UserAnimeListFragment.userAnimeList.size();
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
 
@@ -49,33 +43,37 @@ public class UserProfileFragment extends Fragment {
         name.setText(instance.getUsername());
 
         // Displays username in profile page
-        TextView username = view.findViewById(R.id.usernameProfile);
+        TextView username = view.findViewById(R.id.profile_name);
         username.setText(instance.getUsername());
 
         // Displays email in profile page
         TextView email = view.findViewById(R.id.emailProfile);
-        email.setText(instance.getEmail());
+        email.setText("  Email Address: " + instance.getEmail());
 
         // Displays birthday in profile page
         TextView birthday = view.findViewById(R.id.birthdayProfile);
-        birthday.setText("DOB : " + instance.getBirthday());
+        birthday.setText("  Date of Birth : " + instance.getBirthday());
 
         // Displays gender in profile page
         TextView gender = view.findViewById(R.id.genderProfile);
-        gender.setText(instance.getGender());
+        gender.setText("  Gender: " + instance.getGender());
 
         // Displays phone number in profile page
         TextView phoneNumber = view.findViewById(R.id.phoneNumberProfile);
-        phoneNumber.setText("Phone: " + instance.getPhoneNumber());
+        phoneNumber.setText("  Phone: " + instance.getPhoneNumber());
 
         // Get current date and time and displays it in user profile
         Date date = new Date();
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
         String strDate= formatter.format(date);
 
         // Displays current date in user profile page
         TextView displayDate = view.findViewById(R.id.dateProfile);
-        displayDate.setText("Acc. Created: "+ strDate);
+        displayDate.setText("  Account Created: " + strDate);
+
+        // Displays gender in profile page
+        TextView numberOfAnimes = view.findViewById(R.id.numberOfAnimesProfile);
+        numberOfAnimes.setText("  Number of Animes in your list : " + numberOfUserAnimes);
 
         // Sign out button that goes to sign in screen
         Button goToLoginPage = view.findViewById(R.id.signOutButton);
@@ -83,6 +81,7 @@ public class UserProfileFragment extends Fragment {
             Intent intent = new Intent(v.getContext(), SignInScreen.class);
             v.getContext().startActivity(intent);
         });
+
 
         return view;
     }
